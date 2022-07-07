c688bda3ddda1b1fa6c5c06e7bae4bd7ce87c3f3
nurgul2204 <97363108+nurgul2204@users.noreply.github.com>
2022-07-07 17:08:32 +0100

    CukesRunner updated

--- src/test/java/com/cydeo/runners/CukesRunner.java
+++ src/test/java/com/cydeo/runners/CukesRunner.java
@@ -6,8 +6,16 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-    features = "src/test/resources/features",
-        glue = "com/cydeo/step_definitions"
+        plugin = {
+                "html:target/cucumber-reports.html",
+                "rerun:target/rerun.txt",
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber",
+        },
+        features = "src/test/resources/features",
+        glue = "com/cydeo/step_definitions",
+        dryRun = false,
+        tags = "",
+        publish = true
 
 )
 
